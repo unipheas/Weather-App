662cfb7f32ea9396ea2178ed592394b5a9d0573f
Brian Phillips <2313265939@qq.com>
2016-10-21 17:57:55 +0800

    change DOM with temp,loc,weather

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   $.getJSON('http://ipinfo.io', function(data){
-  console.log(data);
     $.getJSON('http://api.openweathermap.org/data/2.5/weather?APPID=c804a18bf3ddca13b347738ba07a1640&q='+data.city, function(info){
-      console.log(info);
-      console.log(info.main.temp);
+      $("#temp").html(info.main.temp);
+      $("#loc").html(info.name);
+      $("#weather").html(info.weather[0].main);
     });
   });
 })
